65ea09c2383874902ae5f85bd933d62f0189ffc7
joseporres <jose.porres@utec.edu.pe>
2023-08-24 19:54:11 -0500

    feat: hash-load-balancer v1

---
+++ hash-load-balancer.js
@@ -0,0 +1,39 @@
+const http = require("http");
+const crypto = require("crypto");
+
+const backendServers = [
+  { address: "localhost", port: 3000 },
+  { address: "localhost", port: 3001 },
+  { address: "localhost", port: 3002 },
+];
+
+function hashServer(key) {
+  const hash = crypto.createHash("md5").update(key).digest("hex");
+  const serverIndex = parseInt(hash, 16) % backendServers.length;
+  return backendServers[serverIndex];
+}
+
+const proxyServer = http.createServer((req, res) => {
+  const clientIP = req.connection.remoteAddress;
+  const selectedServer = hashServer(clientIP);
+
+  const proxyRequest = http.request(
+    {
+      host: selectedServer.address,
+      port: selectedServer.port,
+      path: req.url,
+      method: req.method,
+      headers: req.headers,
+    },
+    (proxyResponse) => {
+      res.writeHead(proxyResponse.statusCode, proxyResponse.headers);
+      proxyResponse.pipe(res, { end: true });
+    }
+  );
+
+  req.pipe(proxyRequest, { end: true });
+});
+
+proxyServer.listen(8080, () => {
+  console.log("Proxy server listening on port 8080");
+});
